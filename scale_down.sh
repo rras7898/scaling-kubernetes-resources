dd83bf3f12c62532f619d8a07da93151bc9ea1d5
Rohit R <rohit@Arpits-MacBook-Pro.local>
2024-10-15 15:59:15 +0530

    test-4

--- scale_down.sh
+++ scale_down.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
 
+gcloud auth activate-service-account --key-file=./serviceaccount-${BUILD_NUMBER}.json
+gcloud container clusters get-credentials cluster-1 --zone us-central1-c --project apt-bpl
+context=gke_apt-bpl_us-central1-c_cluster-1
+
+
+###################################################################
+
+if [ "$env" == 'dict' ]
+then
+
+    kubectl -n dict scale deployment --all --replicas=0 --context $context
+    echo "Digipass has been scaled down now"
+
+
+fi    
+
+
+# if [ "$env" == 'gcanada' ]
+# then
+
+#     env_name="GoogleCanada"
+
+#     kubectl -n gcanada scale deployment --all --replicas=1 --context $context
+#     echo "$env_name has been scaled up now"
+
+
+# fi
